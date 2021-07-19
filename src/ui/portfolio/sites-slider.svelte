4976c2d45e44730193884ef3866648cd7200861a
eminqasimov <eminqasimov@outlook.com>
2021-07-19 21:34:44 +0400

    fb enabled

--- src/ui/portfolio/sites-slider.svelte
+++ src/ui/portfolio/sites-slider.svelte
@@ -2,12 +2,12 @@
 	import Frame from './frame.svelte';
 
 	export let framesData = [];
-	export let activeIndex = 0;
+	export let activeIndex;
 </script>
 
 <div class="iframes">
 	{#each framesData as frame, index}
-		{#if activeIndex === index}
+		{#if activeIndex == index}
 			<div class="frame-wrapper">
 				<Frame {...frame} />
 			</div>
